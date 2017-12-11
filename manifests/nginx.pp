f7f7d99a6dfa380474c755114ebc420e09c64020
root <root@shashiudawa4.mylabserver.com>
2017-12-11 06:18:04 +0000

    nginx pro

--- manifests/nginx.pp
+++ manifests/nginx.pp
@@ -1,11 +1,10 @@
 class profile::nginx(
-Array $vhosts,
+  #Array $vhosts,
 ) {
   include ::nginx
-  $vhosts.each |$vhost| {
-    ::nginx::vhost { $vhost['fqdn']:
-      port           => $vhost['port'],
-      server_aliases => $vhost['server_aliases'],
+  #$vhosts.each |$vhost| {
+    ::nginx::vhost { $facts['fqdn']:
+      #port           => $vhost['port'],
+      #server_aliases => $vhost['server_aliases'],
     }
-  }
 }
